cf576b10aff43c4ff7125de39bd11e85c60eb9db
e1b19099 <e1b19099@oit.ac.jp>
2021-10-12 14:08:36 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec03/lec03springboot/Lec03springbootApplication.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.lec03springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec03springbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec03springbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec03/lec03springboot/Lec03springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec03.lec03springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec03springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
